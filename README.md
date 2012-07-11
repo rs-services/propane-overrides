53fb5a03e186ea0eb6facae3c4ac8ea6031a429d
brett <brc@rightscale.com>
2012-07-11 16:20:39 -0700

    Initial commit
    To fix CloudDNS region input bug

---
+++ README.md
@@ -0,0 +1,4 @@
+Description
+===========
+RightScale Cookbooks Override
+
---
+++ cookbooks/sys_dns/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "RightScale, Inc."
+maintainer_email "ps@rightscale.com"
+license          "Copyright RightScale, Inc. All rights reserved."
+description      "Installs/Configures sys_dns"
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "12.1.0"
